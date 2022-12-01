75582a38ad665d778cebf25b9c8f7c89a11c36de
mdsoliman-me <contact.mdsoliman@gmail.com>
2022-12-01 22:58:13 +0600

    added edit button from array

--- src/component/Main.jsx
+++ src/component/Main.jsx
@@ -1,46 +1,50 @@
 import React from "react";
 import "./style/main.scss";
 const Main = () => {
-    const filterElement = [
-        {
-            name: "Brightness",
-        },
-        {
-            name: "Brightness",
-        },
-        {
-            name: "Brightness",
-        },
-        {
-            name: "Brightness",
-        },
-        {
-            name: "Brightness",
-        },
-        {
-            name: "Brightness",
-        },
-    ]
+  const filterElement = [
+    {
+      name: "Brightness",
+    },
+    {
+      name: "Grayscale",
+    },
+    {
+      name: "Sepia",
+    },
+    {
+      name: "Saturate",
+    },
+    {
+      name: "Contrast",
+    },
+    {
+      name: "HueRotate",
+    },
+  ];
 
-  return <div className="image__editor">
-    <div className="card">
+  return (
+    <div className="image__editor">
+      <div className="card">
         <div className="card__header">
-            <h2>_______Image Editor _______</h2>
+          <h2>_______Image Editor _______</h2>
         </div>
         <div className="card__body">
-            <div className="sidebar">
-                <div className="side__body">
-                    <div className="filter__section">
-                        <span>Filters</span>
-                        <div className="filter__key">
-
-                        </div>
-                    </div>
+          <div className="sidebar">
+            <div className="side__body">
+              <div className="filter__section">
+                <span>Filters</span>
+                <div className="filter__key">
+                  {filterElement.map((element, index) => (
+                    <button key={index}>{element.name}</button>
+                  ))}
                 </div>
+              </div>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default Main;
